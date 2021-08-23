318a857585ade8f5795652e9ae24558acf513d92
kenfelix <kmrapper.kf@gmail.com>
2021-08-23 02:46:49 -0700

    update commit

---
+++ pbetty_check.c
@@ -0,0 +1,17 @@
+#include "shell.h"
+
+/**
+ * main - prints 0 to 9
+ * Return: zero
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		printf("this is number %d\n", i);
+	}
+	return (0);
+}
