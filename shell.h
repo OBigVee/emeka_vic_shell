0a8f8bd9dfc79f030d1ba7b04e0ca10ac98066a6
kenfelix <kmrapper.kf@gmail.com>
2021-08-24 13:34:23 -0700

    best update ever

--- shell.h
+++ shell.h
@@ -1,11 +1,69 @@
-#ifndef SHELL_H
-#define SHELL_H
+#ifndef SHELL
+#define SHELL
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <dirent.h>
+#include <stddef.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#define TOKENS_BUFFER_SIZE 64
+#define LINE_SIZE 1024
+#define TOKEN_DELIMITERS " \t\r\n\a"
 
-char **split_line(char *line)
-char **split_line(char *line)
+extern char **environ;
+
+/**
+ * struct builtins - Has builtins and associated funcs
+ * @arg: Builtins name
+ * @builtin: Mathcing builtin func
+ */
+
+typedef struct builtins
+{
+	char *arg;
+	void (*builtin)(char **args, char *line, char **env);
+} builtins_t;
+
+int main(int ac, char **av, char **env);
+
+char *get_line(void);
+
+char **split_line(char *line);
+
+int execute_prog(char **args, char *line, char **env, int flow);
+
+int check_for_builtins(char **args, char *line, char **env);
+
+int launch_prog(char **args);
+
+void exit_shell(char **args, char *line, char **env);
+
+void env_shell(char **args, char *line, char **env);
+
+int _strcmp(char *s1, char *s2);
+
+char *find_path(char *args, char *tmp, char *er);
+
+char *search_cwd(char *filename, char *er);
+
+int be_sure(char *check, char **args);
+
+void shell_prompt(void);
+
+int builtins_checker(char **args);
+
+char *save_path(char *tmp, char *path);
+
+char *read_dir(char *er, struct dirent *s, char *fi, int l, char *p, char *t);
+
+char *_getenv(char *env);
+
+char *_strstr(char *haystack, char *needle);
+
+int _strlen(char *s);
+
 #endif
