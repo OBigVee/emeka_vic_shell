7506fb3e5c52c1a649bc7dc158aed6ed01004906
Victor Adedeji <adedejivictor98@gmail.com>
2021-08-28 00:13:34 +0100

    scalfold header file

---
+++ sshell.h
@@ -0,0 +1,9 @@
+#ifndef SHELL_H                                                   
+#define SHELL_H                                                   
+                                                                  
+void shell_prompt1(void);                                         
+void shell_prompt2(void);                                         
+                                                                  
+char *read_cmd(void);                                             
+                                                                  
+#endif 
