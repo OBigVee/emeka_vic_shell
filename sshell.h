865c736c02003c2f68a0f9a07b8eff5aa18719d4
Victor Adedeji <adedejivictor98@gmail.com>
2021-08-30 11:01:48 +0100

    Delete sshell.h

--- sshell.h
+++
@@ -1,9 +0,0 @@
-#ifndef SHELL_H                                                   
-#define SHELL_H                                                   
-                                                                  
-void shell_prompt1(void);                                         
-void shell_prompt2(void);                                         
-                                                                  
-char *read_cmd(void);                                             
-                                                                  
-#endif 
